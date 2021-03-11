b323db296645907053acaf2446ee83f8f43decd2
P. A. Perez-Toro <36645902+PauPerezT@users.noreply.github.com>
2021-03-11 11:47:25 +0100

    Lab 0

---
+++ Introduccion_Python/Funciones/funciones_ejemplo.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def funcion1(arg1):
+    print("Esta funcion multiplica por 2")
+    return 2*arg1
+def funcion2(arg1):
+    print("Esta funcion multiplica por 3")
+    return 3*arg1
